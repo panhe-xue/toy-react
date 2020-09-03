1622c868b04040a88c6565427c5a00bcc2110983
panhe <panhe@bwoil.com>
2020-09-03 09:05:39 +0800

    done

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 
 function CreateElement(tagName, attribute, children) {
-  // const tag = document.createElement(tagName)
-  // for(let attr in attribute ) {
-  //   tag.setAttribute(attr, attribute[attr])
-  // }
-  // if(Object.prototype.toString.call() === "[object String]") {
-  //   return tag.appendChild(children)
-  // } else {
+  const tag = document.createElement(tagName)
+  for(let attr in attribute ) {
+    tag.setAttribute(attr, attribute[attr])
+  }
+  if(Object.prototype.toString.call() === "[object String]") {
+    return tag.appendChild(children)
+  } else {
 
-  // }
+  }
 }
 
 const a = (<div class="name">
-  <div>aaa</div>
-  <div>vvv</div>
-  <div>bbb</div>
+    <div>aaa</div>
+    <div>vvv</div>
+    <div>bbb</div>
   </div>
 )
